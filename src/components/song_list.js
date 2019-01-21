c766afec8d385b2ca9c7d746bc7a5a5a713ce01a
Oleksandr Dykun <sashaluckyman@gmail.com>
2019-01-20 23:52:07 -0800

    done redux state is rendered on songs app

--- src/components/song_list.js
+++ src/components/song_list.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectSong } from '../actions';
 
 class SongList extends Component {
+
+    renderList() {
+        return this.props.songs.map((song)=>{
+            return (
+                <div key={song.title} className="item">
+                    <div className="right floated content">
+                        <button onClick={() => this.props.selectSong(song)} className="ui button primary">
+                            Select
+                        </button>
+                    </div>
+                    <div className="content">{song.title}</div>
+                </div>
+            );
+        })
+    }
+
     render(){
-        console.log('this.props', this.props)
+        
         return(
-            <div>SongList</div>
+            <div className="ui divided list">{this.renderList()}</div>
         )
     }
 }
 
 const mapStateToProps = (state) => {
-    
     return {songs: state.songs};
 }
 
-export default connect(mapStateToProps) (SongList);
+export default connect(mapStateToProps, {
+    selectSong: selectSong
+}) (SongList);
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,7 +6,7 @@ const songsReducer = () => {
         {title: 'No Scrubs', duration: '4:05'},
         {title: 'Macarena', duration: '2:30'},
         {title: 'All Star', duration: '3:15'},
-        {title: 'Macarena', duration: '1:45'}
+        {title: 'I Want it That Way', duration: '1:45'}
     ]
 }
 
